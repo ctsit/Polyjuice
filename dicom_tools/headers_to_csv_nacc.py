149da92f9e57b2b36f599872fef9f0265bf2eff3
Samantha Emerson <s.emerson@ufl.edu>
2020-05-06 18:09:38 -0400

    Add a second basic csv writer for redcap import

--- dicom_tools/headers_to_csv_nacc.py
+++ dicom_tools/headers_to_csv_nacc.py
@@ -34,7 +34,7 @@ def main():
             ds = dcmread(dcm_file)
 
             data = (
-                    ["PatientID", ds.get("PatientID", "None")],
+                    ["PatientID", ds.get("PatientID", "None")],  # (Put your internal PTID in this tag, we will extract it and replace with NACCID)
                     ["SeriesInstanceUID", ds.get("SeriesInstanceUID", "None")],
                     ["StudyInstanceUID", ds.get("StudyInstanceID", "None")],
                     ["InstanceNumber", ds.get("InstanceNumber", "None")],
@@ -44,45 +44,10 @@ def main():
                     ["Manufacturer", ds.get("Manufacturer", "None")],
                     ["ManufacturerModelName", ds.get("ManufacturerModelName", "None")]
                     )
-# Patient ID (Put your internal PTID in this tag, we will extract it and replace with NACCID)
-# Series instance ID
-# Study instance ID
-# Instance number
-# Series description
-# Study date (min: 1/1/2000, max: current date)
-# Magnetic field strength
-# Manufacturer
-# Manufacturer model name
 
             data_dictionary = {}
             final = convert_tuple_to_dict(data, data_dictionary)
             writer.writerow(final)
-
-
-# fieldnames on redcap:
-# ptid
-# redcap_event_name
-# image_type
-# img_mri_patient_name
-# img_mri_patient_id
-# img_mri_date
-# img_mri_study_descrip
-# img_mri_modality
-# img_mri_ref_physician
-# img_mri_inst
-# img_mri_site
-# img_pet_tracer
-# img_pet_patient_name
-# img_pet_patient_id
-# img_pet_date
-# img_pet_study_descrip
-# img_pet_modality
-# img_pet_ref_physician
-# img_pet_inst
-# img_pet_site
-# imaging_metadata_complete
-
-# it looks like for the REDCap csv output we do not want to look at every file in every folder (no instance number + only one row per PTID/visit) - only the first file for the visit. does every folder have a file called '1.dcm'? i could specify to look at that one rather than walking through every file or figuring out how to stop once it finds the first .dcm file and move to the next folder.
 
 
 if __name__ == '__main__':
