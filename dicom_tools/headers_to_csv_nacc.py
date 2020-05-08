fc0cd6915feb6d1b1f97b3b00fc89af8b2f5f5df
Samantha Emerson <s.emerson@ufl.edu>
2020-05-08 16:46:39 -0400

    Modify the readers to print to csv with proper formatting and sorting strategies

--- dicom_tools/headers_to_csv_nacc.py
+++ dicom_tools/headers_to_csv_nacc.py
@@ -3,6 +3,7 @@
 import os
 import os.path
 import csv
+import sys
 from pydicom import dcmread
 
 
@@ -11,8 +12,55 @@ def convert_tuple_to_dict(tup, di):
     return di
 
 
+def search_directory(parent_file: str, out_dir: str, dicom_folders: list, writer) -> list:
+    if os.path.isfile(parent_file):
+        try:
+            # Send files to be read
+            read_files(parent_file, writer)
+        except Exception as e:
+            print("{} failed".format(parent_file))
+            print(str(e))
+
+    else:
+        for path, dirs, files in os.walk(parent_file):
+            for name in files:
+                try:
+                    working_file = os.path.join(path, name)
+                    # Send files to be read
+                    read_files(working_file, writer)
+                except Exception as e:
+                    print("{} failed".format(name))
+                    print(str(e))
+
+
+def read_files(dcm_file, writer):
+    ds = dcmread(dcm_file)
+
+    data = (
+            ["PatientID", ds.get("PatientID", "None")],
+            # (Put your internal PTID in this tag,
+            # we will extract it and replace with NACCID)
+            ["SeriesInstanceUID", ds.get("SeriesInstanceUID", "None")],
+            ["StudyInstanceUID", ds.get("StudyInstanceID", "None")],
+            ["InstanceNumber", ds.get("InstanceNumber", "None")],
+            ["SeriesDescription", ds.get("SeriesDescription", "None")],
+            ["StudyDate", ds.get("StudyDate", "None")],
+            # (min: 1/1/2000, max: current date)
+            ["MagneticFieldStrength", ds.get("MagneticFieldStrength", "None")],
+            ["Manufacturer", ds.get("Manufacturer", "None")],
+            ["ManufacturerModelName", ds.get("ManufacturerModelName", "None")]
+            )
+
+    data_dictionary = {}
+    final = convert_tuple_to_dict(data, data_dictionary)
+    writer.writerow(final)
+
+
 def main():
-    with open('dicom_tools/output.csv', 'w+', newline='') as output:
+    input_directory = sys.argv[1]
+    output_file = sys.argv[2]
+
+    with open(output_file, 'w+', newline='') as output:
         fieldnames = ["PatientID",
                       "SeriesInstanceUID",
                       "StudyInstanceUID",
@@ -23,33 +71,10 @@ def main():
                       "Manufacturer",
                       "ManufacturerModelName"]
         writer = csv.DictWriter(output, fieldnames=fieldnames)
-        dcm_files = []
-        for root, dirs, files in os.walk('/Users/s.emerson/Documents/Code/polyjuice_examplefiles/input'):
-            for names in files:
-                if names.endswith(".dcm"):
-                    dcm_files.append(os.path.join(root, names))
-
-        for dcm_file in dcm_files:
-            ds = dcmread(dcm_file)
-
-            data = (
-                    ["PatientID", ds.get("PatientID", "None")],
-                    # (Put your internal PTID in this tag,
-                    # we will extract it and replace with NACCID)
-                    ["SeriesInstanceUID", ds.get("SeriesInstanceUID", "None")],
-                    ["StudyInstanceUID", ds.get("StudyInstanceID", "None")],
-                    ["InstanceNumber", ds.get("InstanceNumber", "None")],
-                    ["SeriesDescription", ds.get("SeriesDescription", "None")],
-                    ["StudyDate", ds.get("StudyDate", "None")],
-                    # (min: 1/1/2000, max: current date)
-                    ["MagneticFieldStrength", ds.get("MagneticFieldStrength", "None")],
-                    ["Manufacturer", ds.get("Manufacturer", "None")],
-                    ["ManufacturerModelName", ds.get("ManufacturerModelName", "None")]
-                    )
-
-            data_dictionary = {}
-            final = convert_tuple_to_dict(data, data_dictionary)
-            writer.writerow(final)
+        writer.writeheader()
+        dcm_folders = []
+        # Take a stroll through the folders
+        search_directory(input_directory, output_file, dcm_folders, writer)
 
 
 if __name__ == '__main__':
